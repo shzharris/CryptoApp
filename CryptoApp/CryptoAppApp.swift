4a026102c57612b9063dd93c186d398cf2b4505e
Harris.Mac <shaohaizhou36@gmail.com>
2025-06-27 10:35:13 +0800

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by HarrisShao on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
