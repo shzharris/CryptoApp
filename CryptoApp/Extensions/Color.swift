4e5bb3e9bd449c738d0011961b5b1de8b81eaf89
Harris.Mac <shaohaizhou36@gmail.com>
2025-06-27 12:18:43 +0800

    项目初始化颜色

---
+++ CryptoApp/Extensions/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by HarrisShao on 6/27/25.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondText = Color("SecondTextColor")
+}
